89690a85d212f6e3a7167a0ef0c9cf125ce949f8
Yurii Panasuik <thoryur@gmail.com>
2024-01-21 22:44:48 +0200

    bfs

---
+++ src/main/java/algorithms/bfs/Vertex.java
@@ -0,0 +1,19 @@
+package algorithms.bfs;
+
+
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@Data
+@ToString(exclude = {"neighbors"})
+public class Vertex<T> {
+    private final T data;
+    public Vertex(T data) {
+        this.data = data;
+    }
+    private boolean visited;
+    private List<Vertex<T>> neighbors = new LinkedList<>();
+}
