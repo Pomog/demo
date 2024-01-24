e07cb5678779b8d07124d12c767d8ebba34e215b
Yurii Panasuik <thoryur@gmail.com>
2024-01-24 23:58:15 +0200

    ROOTING 2

--- src/main/java/algorithms/dfs/TreeRooter.java
+++ src/main/java/algorithms/dfs/TreeRooter.java
@@ -2,11 +2,13 @@ package algorithms.dfs;
 
 import algorithms.Vertex;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TreeRooter<T> {
     private Integer rootIndex;
     private List<Vertex<T>> graph;
+    private List<List<Vertex<T>>> paths = new ArrayList<>();
 
     public TreeRooter(Integer rootIndex, List<Vertex<T>> graph) {
         this.rootIndex = rootIndex;
@@ -21,11 +23,29 @@ public class TreeRooter<T> {
 
     private Vertex<T> buildTree (List<Vertex<T>> graph, Integer currentNodeIndex, Vertex<T> parentNode){
         for (Vertex<T> childNode : graph.get(currentNodeIndex).getNeighbors()){
+            List<Vertex<T>> currentPath = new ArrayList<>() {{
+                add(graph.get(currentNodeIndex));
+                add(childNode);
+            }};
+            System.out.println("Now is: " + graph.get(currentNodeIndex) + " ---> " + childNode);
+            List<Vertex<T>> currentPathREV = new ArrayList<>() {{
+                add(childNode);
+                add(graph.get(currentNodeIndex));
+            }};
 
             // Avoid adding an edge pointing back to the parent
-            if (parentNode != null && childNode == parentNode || (parentNode != null && childNode.isVisited()) && parentNode.isVisited()){
+            if (parentNode != null && childNode == parentNode)
+             {
+                 System.out.println("Not included - parentNode");
                 continue;
             }
+            if ((paths.contains(currentPath)) || (paths.contains(currentPathREV))) {
+                System.out.println("Not included - PATH exist");
+                continue;
+            }
+            System.out.println(paths);
+
+            paths.add(currentPath);
 
             childNode.setVisited(true);
 
