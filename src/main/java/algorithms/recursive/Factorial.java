e7111ec65c5721ab93e9edb99e7fcd1a1041bdfa
Yurii Panasuik <thoryur@gmail.com>
2024-02-06 21:18:39 +0200

    getSubsequences

--- src/main/java/algorithms/recursive/Factorial.java
+++ src/main/java/algorithms/recursive/Factorial.java
@@ -3,8 +3,9 @@ package algorithms.recursive;
 public class Factorial {
     public static void main(String[] args) {
         int n = 11;
-        System.out.println(calculateFactorial(n));
-        System.out.println(calculateFibonacci(n));
+//        System.out.println(calculateFactorial(n));
+//        System.out.println(calculateFibonacci(n));
+        System.out.println(getSubsequences("abc"));
 
     }
 
@@ -33,5 +34,36 @@ public class Factorial {
             return -1; // Fibonacci series is not defined for negative numbers
         }
         return calculateFibonacci(n - 1) + calculateFibonacci(n - 2);
+    }
+
+    /**
+     * Returns all possible subsequences from the input string.
+     * @param word The input string from which subsequences are generated.
+     * @return A string containing all possible subsequences of the input string.
+     */
+    private static String getSubsequences(String word){
+        if (word.isEmpty()){
+            return "";
+        }
+
+        char firstChar = word.charAt(0);
+
+        // Recursive call to get the subsequences of the substring starting from index 1
+        String rest = getSubsequences(word.substring(1));
+
+        StringBuilder result = new StringBuilder();
+
+        // Iterate over each subsequence in the rest and concatenate with the first character
+        for (String subSequence : rest.split(",", -1)){
+            // Append the first character of the word followed by the subsequence
+            result.append(",").append(firstChar).append(subSequence);
+            // Append the subsequence alone
+            result.append(",").append(subSequence);
+        }
+
+        // Remove the leading comma before returning the result
+        return  result.substring(1);
+
+
     }
 }
