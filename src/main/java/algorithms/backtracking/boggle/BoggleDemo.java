753b71e0ae39294c6fbd842ba7567a3d1c44663f
Yurii Panasuik <thoryur@gmail.com>
2024-02-18 00:22:49 +0200

    BoggleDemo start

---
+++ src/main/java/algorithms/backtracking/boggle/BoggleDemo.java
@@ -0,0 +1,19 @@
+package algorithms.backtracking.boggle;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class BoggleDemo {
+    static final List<String> dictionary = Arrays.asList(
+            "RAT", "NEAT", "HUG", "RATS", "GUN", "BEAN", "BOAT", "QUILT"
+    );
+    public static void main(String[] args) {
+        char[][] boggle = {
+                {'R', 'S', 'T', 'L'},
+                {'N', 'E', 'A', 'O'},
+                {'H', 'G', 'U', 'B'},
+                {'Z', 'X', 'Y', 'Q'}
+        };
+        
+    }
+}
