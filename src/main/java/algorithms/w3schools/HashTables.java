87120896fb31962151bac46bcf0092189c7b5d65
Yurii Panasuik <thoryur@gmail.com>
2024-03-29 12:05:53 +0200

    HashTables

--- src/main/java/algorithms/w3schools/HashTables.java
+++ src/main/java/algorithms/w3schools/HashTables.java
@@ -1,5 +1,8 @@
 package algorithms.w3schools;
 
+import java.util.EnumMap;
+import java.util.Map;
+
 public class HashTables {
 
     static final String[] myHashSet = {null, "Jones", null, "Lisa", null, "Bob", null, "Siri", "Pete", null};
@@ -8,6 +11,13 @@ public class HashTables {
         System.out.println("'" + value + "' has hash code: " + hashFunction(value));
 
         System.out.println("'Pete' is in the Hash Set: " + contains("Pete"));
+        
+        Map<MyEnum, String> myEnumHashMap = new EnumMap<>(MyEnum.class);
+        myEnumHashMap.put(MyEnum.MONDAY, "First");
+        myEnumHashMap.put(MyEnum.FRIDAY, "Fifth");
+        
+        System.out.println(myEnumHashMap + "\n size =" + myEnumHashMap.size() );
+        
     }
 
     public static int hashFunction(String value) {
---
+++ src/main/java/algorithms/w3schools/MyEnum.java
@@ -0,0 +1,12 @@
+package algorithms.w3schools;
+
+public enum MyEnum {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+
+}
