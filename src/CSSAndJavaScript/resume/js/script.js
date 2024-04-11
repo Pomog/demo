bfab3422490a2d0c1ddce519d990cdedeb43e019
Yurii Panasuik <thoryur@gmail.com>
2024-04-11 23:34:14 +0300

    working on resume

--- src/CSSAndJavaScript/resume/js/script.js
+++ src/CSSAndJavaScript/resume/js/script.js
@@ -1,5 +1,6 @@
 const body = document.body;
 const photo = document.getElementById("photo");
+const endpoint = "server URL"
 
 const shake1 = "shake1";
 const shake2 = "shake2";
@@ -18,18 +19,28 @@ function shakePhoto() {
     photo.classList.add(shake2);
 }
 
-function saveNameToLocalStorage(id){
+function saveClickedProjectToLocalStorage(id){
+    if (localStorage.getItem('ClickedProject:') == null){
+        localStorage.setItem('ClickedProject:', "");
+    }
+
     let projects = localStorage.getItem('ClickedProject:').toString() + ` | ` + id.toString();
     localStorage.setItem('ClickedProject:', projects);
+}
+
+function saveCurrentTimeToLocalStorage(){
+    localStorage.setItem('Data:', new Date().toString());
+}
+
+
+function purgeSentActivity(){
+    localStorage.clear()
 }
 
 /**
  * IIFE. Retrieves the current geographical position and stores latitude and longitude in localStorage.
  */
 (function() {
-    console.log(this);
-    console.log(navigator);
-
     // Get the current geographical position
     navigator.geolocation.getCurrentPosition(
         function(position) {
@@ -48,19 +59,36 @@ function saveNameToLocalStorage(id){
  * Sends the data from Window.localStorage to the server endpoint using fetch.
  */
 async function sendDataToServer(endpoint) {
+    saveCurrentTimeToLocalStorage();
     try {
-        await fetch(endpoint, {
+        let response = await fetch(endpoint, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(localStorage)
         });
+        if (!response.ok) {
+            console.log('The server response is: ', response.status);
+        }
 
         console.log('Data sent to server successfully');
+        purgeSentActivity();
+    } catch (error) {
+        console.error('Error sending data to server:', error);
+        purgeSentActivity(); // TODO: only for test, REMOVE THIS
+    }
+}
+
+async function handleUnload() {
+    console.log('Page is about to be closed');
+    try {
+        await sendDataToServer(endpoint);
     } catch (error) {
         console.error('Error sending data to server:', error);
     }
 }
+
+window.addEventListener('unload', handleUnload);
 
 
