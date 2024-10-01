80923738e2ed43c0271848c0b82f303d4e61c6d6
Yurii Panasuik <thoryur@gmail.com>
2024-10-01 11:47:31 +0300

    dijkstra

--- src/main/java/algorithms/greedy/Dijkstra_Udemy.java
+++ src/main/java/algorithms/greedy/Dijkstra_Udemy.java
@@ -1,4 +1,59 @@
 package algorithms.greedy;
 
 public class Dijkstra_Udemy {
+    public static void main(String[] args) {
+        int[][] adjacencyMatrix = new AdjMatrixFactory().createAdjacencyMatrix();
+        
+        dijkstra(adjacencyMatrix);
+    }
+    
+    private static void dijkstra(int[][] adjacencyMatrix) {
+        
+        int numberOfVertices = adjacencyMatrix.length;
+        
+        boolean[] visitedNodes = new boolean[numberOfVertices];
+        
+        int[] distances = new int[numberOfVertices];
+
+//        set distance for source as 0
+        distances[0] = 0;
+
+//        set infinity distances to all other Nodes
+        for (int i = 1; i < numberOfVertices; i++) {
+            distances[i] = Integer.MAX_VALUE;
+        }
+
+//      find the vertex with Min distance
+        for (int i = 0; i < numberOfVertices - 1; i++) {
+            int vertexWithMinDistance = findMinVertex(distances, visitedNodes);
+            
+            visitedNodes[vertexWithMinDistance] = true;
+            
+            for (int j = 0; j < numberOfVertices; j++) {
+                if (adjacencyMatrix[vertexWithMinDistance][j] != 0 && !visitedNodes[j]) {
+                    int newDistance = distances[vertexWithMinDistance] + adjacencyMatrix[vertexWithMinDistance][j];
+                    if (newDistance < distances[j]) {
+                        distances[j] = newDistance;
+                    }
+                }
+            }
+        }
+        
+        for (int i=0; i<numberOfVertices; i++){
+            System.out.println(i + " "+ distances[i]);
+        }
+    }
+    
+    private static int findMinVertex(int[] distances, boolean[] visitedNodes) {
+        int vertexWithMinDistance = -1;
+        
+        for (int i = 0; i < distances.length; i++) {
+            if (!visitedNodes[i] && (vertexWithMinDistance == -1 || distances[i] < distances[vertexWithMinDistance])) {
+                vertexWithMinDistance = i;
+            }
+        }
+        
+        return vertexWithMinDistance;
+        
+    }
 }
