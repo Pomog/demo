1e0fea99ef4a66600885bf1be7e2e9c76cba5362
Yurii Panasuik <thoryur@gmail.com>
2024-10-02 08:46:00 +0300

    BellmanFord

--- src/main/java/algorithms/greedy/BellmanFord.java
+++ src/main/java/algorithms/greedy/BellmanFord.java
@@ -2,18 +2,20 @@ package algorithms.greedy;
 
 import algorithms.named.Edge;
 
+import java.util.Arrays;
+
 public class BellmanFord {
     // number of vertices and edges
     int VERTICES, EDGES;
-    Edge[] edges;
+    Edge[] edge;
     
     public BellmanFord(int VERTICES, int EDGES) {
         this.VERTICES = VERTICES;
         this.EDGES = EDGES;
-        edges = new Edge[EDGES];
+        edge = new Edge[EDGES];
         
         for (int i = 0; i < EDGES; i++) {
-            edges[i] = new Edge();
+            edge[i] = new Edge();
         }
     }
     
@@ -21,42 +23,83 @@ public class BellmanFord {
         BellmanFord graph = new BellmanFord(6 , 9);
 // A=0, B=1, C=2, D=3, E=4, F=5
 //        AB 01
-        graph.edges[0].s = 0;
-        graph.edges[0].d = 1;
-        graph.edges[0].w = 8;
+        graph.edge[0].s = 0;
+        graph.edge[0].d = 1;
+        graph.edge[0].w = 8;
 //        AF 05
-        graph.edges[1].s = 0;
-        graph.edges[1].d = 5;
-        graph.edges[1].w = 5;
+        graph.edge[1].s = 0;
+        graph.edge[1].d = 5;
+        graph.edge[1].w = 5;
 //        AD 03
-        graph.edges[2].s = 0;
-        graph.edges[2].d = 3;
-        graph.edges[2].w = 3;
+        graph.edge[2].s = 0;
+        graph.edge[2].d = 3;
+        graph.edge[2].w = 3;
 //        BC 12
-        graph.edges[3].s = 1;
-        graph.edges[3].d = 2;
-        graph.edges[3].w = 6;
+        graph.edge[3].s = 1;
+        graph.edge[3].d = 2;
+        graph.edge[3].w = 6;
 //        CE 24
-        graph.edges[4].s = 2;
-        graph.edges[4].d = 4;
-        graph.edges[4].w = 4;
+        graph.edge[4].s = 2;
+        graph.edge[4].d = 4;
+        graph.edge[4].w = 4;
 //        DE 34
-        graph.edges[5].s = 3;
-        graph.edges[5].d = 4;
-        graph.edges[5].w = -1;
+        graph.edge[5].s = 3;
+        graph.edge[5].d = 4;
+        graph.edge[5].w = -1;
 //        FB 51
-        graph.edges[6].s = 5;
-        graph.edges[6].d = 1;
-        graph.edges[6].w = -4;
+        graph.edge[6].s = 5;
+        graph.edge[6].d = 1;
+        graph.edge[6].w = -4;
 //        FC 52
-        graph.edges[7].s = 5;
-        graph.edges[7].d = 5;
-        graph.edges[7].w = -1;
+        graph.edge[7].s = 5;
+        graph.edge[7].d = 2;
+        graph.edge[7].w = -1;
 //        FE 54
-        graph.edges[8].s = 5;
-        graph.edges[8].d = 4;
-        graph.edges[8].w = -3;
+        graph.edge[8].s = 5;
+        graph.edge[8].d = 4;
+        graph.edge[8].w = -3;
         
+        bellmanFord(graph, 0);
+    }
+    
+    private static void bellmanFord(BellmanFord graph, int source) {
+        int[] distance = new int[graph.VERTICES];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        distance[source] = 0;
         
+//        Relax edges Vertices-1 times
+        for (int i=0; i<graph.VERTICES; ++i){
+            for (int j=0; j<graph.EDGES; ++j){
+                int sourceVertex = graph.edge[j].s;
+                int destVertex = graph.edge[j].d;
+                int costVertex = graph.edge[j].w;
+                
+                if (
+                    distance[sourceVertex] != Integer.MAX_VALUE
+                            &&
+                            (distance[sourceVertex] + costVertex < distance[destVertex])
+                ){
+                    distance[destVertex] = distance[sourceVertex] + costVertex;
+                }
+            }
+        }
+        
+            for (int j=0; j<graph.EDGES; ++j){
+                int sourceVertex = graph.edge[j].s;
+                int destVertex = graph.edge[j].d;
+                int costVertex = graph.edge[j].d;
+                
+                if (
+                        distance[sourceVertex] != Integer.MAX_VALUE
+                                &&
+                                distance[sourceVertex] + costVertex < distance[destVertex]
+                ){
+                    System.out.println("The Graph contains negative cycle");
+                }
+            }
+        
+        for (int i =0; i<graph.VERTICES; i++){
+            System.out.println(i + " : " + distance[i]);
+        }
     }
 }
