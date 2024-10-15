07b0370ccb239019ee22833f6fe14b9ed9f0693a
Yurii Panasuik <thoryur@gmail.com>
2024-10-15 22:15:18 +0300

    Ford-Fulkerson sTART

---
+++ src/main/java/algorithms/named/ford_fulkerson/Ford_Fulkerson.java
@@ -0,0 +1,12 @@
+package algorithms.named.ford_fulkerson;
+
+public class Ford_Fulkerson {
+    int[][] graph = {
+            {0,3,2,0,0,0},
+            {0,0,0,2,0,0},
+            {0,3,0,0,3,0},
+            {0,0,1,0,0,3},
+            {0,0,0,3,0,2},
+            {0,0,0,0,0,0},
+    };
+}
