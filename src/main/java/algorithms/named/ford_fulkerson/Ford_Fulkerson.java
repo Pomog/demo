0c89697dfee041c026d5e091cd6d89688e4b9a2f
Yurii Panasuik <thoryur@gmail.com>
2024-10-16 22:26:06 +0300

    Ford-Fulkerson cont1

--- src/main/java/algorithms/named/ford_fulkerson/Ford_Fulkerson.java
+++ src/main/java/algorithms/named/ford_fulkerson/Ford_Fulkerson.java
@@ -1,12 +1,42 @@
 package algorithms.named.ford_fulkerson;
 
+import java.util.*;
+
 public class Ford_Fulkerson {
-    int[][] graph = {
-            {0,3,2,0,0,0},
-            {0,0,0,2,0,0},
-            {0,3,0,0,3,0},
-            {0,0,1,0,0,3},
-            {0,0,0,3,0,2},
-            {0,0,0,0,0,0},
-    };
+    public static void main(String[] args) {
+        int[][] graph = {
+                {0, 3, 2, 0, 0, 0},
+                {0, 0, 0, 2, 0, 0},
+                {0, 3, 0, 0, 3, 0},
+                {0, 0, 1, 0, 0, 3},
+                {0, 0, 0, 3, 0, 2},
+                {0, 0, 0, 0, 0, 0}
+        };
+        
+        // Edmonds-Karp BFS
+        getMaxFlow(graph, 0, graph.length);
+    }
+    
+    private static void getMaxFlow(int[][] graph, int source, int sink) {
+        
+        int[][] residualGraph = Arrays.stream(graph)
+                .map(int[]::clone)
+                .toArray(int[][]::new);
+        
+        Map<Integer, Integer> parentMapping = new HashMap<>();
+        List<List<Integer>> augmentingPaths = new ArrayList<List<Integer>>();
+        
+
+        
+    }
+    
+    private static void print(int[][] graph) {
+        Arrays.stream(graph)
+                .map(Arrays::toString)
+                .forEach(System.out::println);
+    }
+    
+    
 }
+
+
